1e6613b9f33c76d6633673440eabdbba771851a6
Mikhaylenko Egor <zhora.mikhaylenko@mail.ru>
2022-05-28 00:49:55 +0700

    made cpp file for header Button.h

---
+++ src/lib/Button.cpp
@@ -0,0 +1,57 @@
+#include "Button.h"
+
+Button::Button(const double _width, const double _height, const double _x, const double _y, const int _character) {
+	SetSize(_width, _height);
+	SetKeyCode(_character);
+	SetPosistion(_x, _y);
+}
+
+void Button::SetSize(const double _width, const double _height) {
+	this->size_.x = _width;
+	this->size_.y = _height;
+}
+
+void Button::SetKeyCode(const int _character) {
+	this->keycode_ = _character;
+}
+
+void Button::SetPosistion(const double _width, const double _height) {
+	this->position_.x = _width;
+	this->position_.y = _height;
+}
+
+void Button::SetCharacter(const char _symbol) {
+	this->character_ = _symbol;
+}
+
+void Button::SetColor(const sf::Color _color) {
+	this->color_ = _color;
+}
+
+void Button::SetCharacterColor(const sf::Color _color) {
+	this->character_color_ = _color;
+}
+
+int Button::GetKeyCode() {
+	return this->keycode_;
+}
+
+char Button::GetCharacter() {
+	return this->character_;
+}
+
+Button::Options Button::GetSize() {
+	return this->size_;
+}
+
+Button::Options Button::GetPosition() {
+	return this->position_;
+}
+
+sf::Color Button::GetColor() {
+	return this->color_;
+}
+
+sf::Color Button::GetCharacterColor() {
+	return this->character_color_;
+}
--- src/lib/Button.h
+++ src/lib/Button.h
@@ -11,67 +11,25 @@ private:
 
 	int keycode_;
 	char character_;
-	Color color_;
-	Color character_color_;
+	sf::Color color_;
+	sf::Color character_color_;
 	Options size_;
 	Options position_;
 
 public:
 
-	Button(const double _width = 60, const double _height = 60, const double _x = 0, const double _y = 0, const int _character = NULL) {
-		SetSize(_width, _height);
-		SetKeyCode(_character);
-		SetPosistion(_x, _y);
-	}
-
-	void SetSize(const double _width, const double _height) {
-		this->size_.x = _width;
-		this->size_.y = _height;
-	}
-
-	void SetKeyCode(const int _character) {
-		this->keycode_ = _character;
-	}
-
-	void SetPosistion(const double _width, const double _height) {
-		this->position_.x = _width;
-		this->position_.y = _height;
-	}
-
-	void SetCharacter(const char _symbol) {
-		this->character_ = _symbol;
-	}
-
-	void SetColor(const Color _color) {
-		this->color_ = _color;
-	}
-
-	void SetCharacterColor(const Color _color) {
-		this->character_color_ = _color;
-	}
-
-	int GetKeyCode() {
-		return this->keycode_;
-	}
-
-	char GetCharacter() {
-		return this->character_;
-	}
-
-	Options GetSize() {
-		return this->size_;
-	}
-
-	Options GetPosition() {
-		return this->position_;
-	}
-
-	Color GetColor() {
-		return this->color_;
-	}
-
-	Color GetCharacterColor() {
-		return this->character_color_;
-	}
+	Button(const double _width = 60, const double _height = 60, const double _x = 0, const double _y = 0, const int _character = 0);
+	void SetSize(const double _width, const double _height);
+	void SetKeyCode(const int _character);
+	void SetPosistion(const double _width, const double _height);
+	void SetCharacter(const char _symbol);
+	void SetColor(const sf::Color _color);
+	void SetCharacterColor(const sf::Color _color);
+	int GetKeyCode();
+	char GetCharacter();
+	Options GetSize();
+	Options GetPosition();
+	sf::Color GetColor();
+	sf::Color GetCharacterColor();
 
 };
