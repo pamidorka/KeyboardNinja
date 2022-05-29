fbdb69708158a45ea7de4f1d9a1da1071db9cde5
Mikhaylenko Egor <zhora.mikhaylenko@mail.ru>
2022-05-30 01:19:17 +0700

    main functions update. minor changes in makefile and gitignore

--- makefile
+++ makefile
@@ -10,13 +10,14 @@ SETTINGS = src/lib/Settings.cpp
 TEXTBOX = src/lib/Textbox.cpp
 
 MAIN = src/main/main.cpp
+
 SFML = -lsfml-graphics -lsfml-window -lsfml-system
 
-COMPILE = obj/lib/main.o obj/lib/Button.o obj/lib/Keyboard.o obj/lib/Settings.o obj/lib/Textbox.o
+COMPILE = obj/main/main.o obj/lib/Button.o obj/lib/Keyboard.o obj/lib/Settings.o obj/lib/Textbox.o
 
 bin/master: $(COMPILE) $(SFML)
 	$(CPP) $(COMPILE) $(SFML) -o $@
-obj/lib/main.o: $(MAIN) $(SFML)
+obj/main/main.o: $(MAIN) $(SFML)
 	$(CPP) -c $(FF) $(CF) $(CPPF) -o $@ $(MAIN)
 obj/lib/Button.o: $(BUTTON) $(SFML)
 	$(CPP) -c $(FF) $(CF) $(CPPF) -o $@ $(BUTTON)
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,25 +1,30 @@
 #include <SFML/Graphics.hpp>
 #include <lib/Keyboard.h>
+#include <lib/Textbox.h>
 
 int main() {
     sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(1200, 600), "Keyboard Ninja", Style::Close);
     VirtualKeyboard* keyboard = new VirtualKeyboard("Roboto-Regular.ttf");
+	Settings* settings = new Settings;
+	Textbox* textbox = new Textbox(settings);
 
-    while (window.isOpen()) {
+    while (window->isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event)) {
+        while (window->pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
-                window.close();
+                window->close();
             }
 			if (event.type == sf::Event::KeyPressed) {
 				keyboard->ChangePressedKey(event.key.code);
+				textbox->MovePointer(event.key.code);
 			}
 			if (event.type == sf::Event::KeyReleased) {
 				keyboard->ChangeReleasedKey(event.key.code);
 			}
 		}
 		window->clear(sf::Color::White);
-		board->Draw(window);
+		keyboard->Draw(window);
+		textbox->Draw(window);
 		window->display();
     }
 
