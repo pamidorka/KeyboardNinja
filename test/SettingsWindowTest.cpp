15bc442188353b08e38e9f8dda09c2202ab71deb
Vadim <91959106+Liswiera@users.noreply.github.com>
2022-06-15 21:35:08 +0700

    Add tests for SettingsWindow class

---
+++ test/SettingsWindowTest.cpp
@@ -0,0 +1,13 @@
+#include "src/lib/Windows/SettingsWindow.h"
+#include "thirdparty/ctest.h"
+
+CTEST(stat_suite_, clock_test) {
+    ASSERT_STR("00:00", SettingsWindow::ClockFormatString(0).c_str());
+	ASSERT_STR("00:05", SettingsWindow::ClockFormatString(5).c_str());
+    ASSERT_STR("00:59", SettingsWindow::ClockFormatString(59).c_str());
+    ASSERT_STR("01:00", SettingsWindow::ClockFormatString(60).c_str());
+    ASSERT_STR("07:30", SettingsWindow::ClockFormatString(450).c_str());
+    ASSERT_STR("15:00", SettingsWindow::ClockFormatString(900).c_str());
+    ASSERT_STR("90:00", SettingsWindow::ClockFormatString(5400).c_str());
+    ASSERT_STR("100:00", SettingsWindow::ClockFormatString(6000).c_str());
+}
