0ddb85ec45883012c85cd328dfef804a2aefa2d6
Vadim <91959106+Liswiera@users.noreply.github.com>
2022-06-15 21:12:06 +0700

    Add tests for Button class

---
+++ test/ButtonTest.cpp
@@ -0,0 +1,83 @@
+#include "src/lib/Buttons/Button.h"
+#include "thirdparty/ctest.h"
+
+CTEST(stat_suite_, clock_test) {
+	Settings s;
+    Button btn(30, 30, 20, 20, nullptr, "", 1, kDefaultColorSet);
+
+    ASSERT_TRUE(btn.IsMouseHover(Vector2f(40, 40)));
+    ASSERT_TRUE(btn.IsMouseHover(Vector2f(48, 32)));
+    ASSERT_FALSE(btn.IsMouseHover(Vector2f(15, 40)))
+    ASSERT_FALSE(btn.IsMouseHover(Vector2f(35, 5)))
+}
+
+CTEST(stat_suite_, clock_test) {
+	Settings s;
+	Button btn(0, 0, 100, 100, nullptr, "", 1, kDefaultColorSet);
+
+    ASSERT_TRUE(btn.GetButtonState() == ButtonStates::ButtonIdle);
+    ASSERT_FALSE(btn.Update(Vector2f(500, 500), false));
+    ASSERT_TRUE(btn.GetButtonState() == ButtonStates::ButtonIdle);
+    ASSERT_FALSE(btn.Update(Vector2f(10, 10), false));
+    ASSERT_TRUE(btn.GetButtonState() == ButtonStates::ButtonHover);
+    ASSERT_FALSE(btn.Update(Vector2f(500, 500), false));
+    ASSERT_TRUE(btn.GetButtonState() == ButtonStates::ButtonIdle);
+}
+
+CTEST(stat_suite_, clock_test) {
+	Settings s;
+	Button btn(0, 0, 100, 100, nullptr, "", 1, kDefaultColorSet);
+
+    ASSERT_FALSE(btn.Update(Vector2f(10, 10), false));
+    ASSERT_TRUE(btn.GetButtonState() == ButtonStates::ButtonHover);
+    ASSERT_FALSE(btn.Update(Vector2f(10, 10), true));
+    ASSERT_TRUE(btn.GetButtonState() == ButtonStates::ButtonHoverActive);
+    ASSERT_TRUE(btn.Update(Vector2f(10, 10), false));
+    ASSERT_TRUE(btn.GetButtonState() == ButtonStates::ButtonHover);
+}
+
+CTEST(stat_suite_, clock_test) {
+	Settings s;
+	Button btn(0, 0, 100, 100, nullptr, "", 1, kDefaultColorSet);
+
+    ASSERT_FALSE(btn.Update(Vector2f(500, 500), true));
+    ASSERT_TRUE(btn.GetButtonState() == ButtonStates::ButtonIdle);
+}
+
+CTEST(stat_suite_, clock_test) {
+	Settings s;
+	Button btn(0, 0, 100, 100, nullptr, "", 1, kDefaultColorSet);
+    
+    ASSERT_FALSE(btn.Update(Vector2f(10, 10), true));
+    ASSERT_TRUE(btn.GetButtonState() == ButtonStates::ButtonHoverInactive);
+    ASSERT_FALSE(btn.Update(Vector2f(10, 10), false));
+    ASSERT_TRUE(btn.GetButtonState() == ButtonStates::ButtonHover);
+}
+
+CTEST(stat_suite_, clock_test) {
+	Settings s;
+	Button btn(0, 0, 100, 100, nullptr, "", 1, kDefaultColorSet);
+    
+    ASSERT_FALSE(btn.Update(Vector2f(10, 10), false));
+    ASSERT_TRUE(btn.GetButtonState() == ButtonStates::ButtonHover);
+    ASSERT_FALSE(btn.Update(Vector2f(10, 10), true));
+    ASSERT_TRUE(btn.GetButtonState() == ButtonStates::ButtonHoverActive);
+    ASSERT_FALSE(btn.Update(Vector2f(500, 10), true));
+    ASSERT_TRUE(btn.GetButtonState() == ButtonStates::ButtonUnhoverActive);
+    ASSERT_FALSE(btn.Update(Vector2f(500, 10), false));
+    ASSERT_TRUE(btn.GetButtonState() == ButtonStates::ButtonIdle);
+}
+
+CTEST(stat_suite_, clock_test) {
+	Settings s;
+	Button btn(0, 0, 100, 100, nullptr, "", 1, kDefaultColorSet);
+    
+    ASSERT_FALSE(btn.Update(Vector2f(10, 10), false));
+    ASSERT_TRUE(btn.GetButtonState() == ButtonStates::ButtonHover);
+    ASSERT_FALSE(btn.Update(Vector2f(10, 10), true));
+    ASSERT_TRUE(btn.GetButtonState() == ButtonStates::ButtonHoverActive);
+    ASSERT_FALSE(btn.Update(Vector2f(500, 10), true));
+    ASSERT_TRUE(btn.GetButtonState() == ButtonStates::ButtonUnhoverActive);
+    ASSERT_FALSE(btn.Update(Vector2f(10, 10), true));
+    ASSERT_TRUE(btn.GetButtonState() == ButtonStates::ButtonHoverActive);
+}
